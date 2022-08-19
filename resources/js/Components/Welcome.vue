ed9266faa065eadd0d42cca5acbe35559d022433
stux <hello@mstdn.social>
2022-08-19 15:33:01 +0200

    Init

---
+++ resources/js/Components/Welcome.vue
@@ -0,0 +1,26 @@
+<script setup>
+import JetApplicationLogo from '@/Components/ApplicationLogo.vue';
+import { Timeline } from 'flowbite-vue';
+import { Card } from 'flowbite-vue';
+</script>
+
+<template>
+    <div>
+        <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
+            <div>
+                <JetApplicationLogo class="block h-12 w-auto" />
+            </div>
+
+            <div class="mt-8 text-2xl">
+                Welcome to your Jetstream application!
+                <Card>All cool?</Card>
+            </div>
+        </div>
+
+
+        <Timeline>
+            
+        </Timeline>
+
+    </div>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,57 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./vendor/laravel/jetstream/**/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.vue",
+        "node_modules/flowbite-vue/**/*.{js,jsx,ts,tsx}",
+        "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
+    ],
+    darkMode: "class", // or 'media' or 'class'
+    theme: {
+        screens: {
+            xs: "614px",
+            sm: "1002px",
+            md: "1022px",
+            lg: "1092px",
+            xl: "1280px",
+        },
+        extend: {
+            animation: {
+                "spin-fast": "spin 0.7s linear infinite",
+            },
+            colors: {
+                dim: {
+                    50: "#5F99F7",
+                    100: "#5F99F7",
+                    200: "#38444d",
+                    300: "#202e3a",
+                    400: "#253341",
+                    500: "#5F99F7",
+                    600: "#5F99F7",
+                    700: "#192734",
+                    800: "#162d40",
+                    900: "#15202b",
+                },
+            },
+            width: {
+                68: "68px",
+                88: "88px",
+                275: "275px",
+                290: "290px",
+                350: "350px",
+                600: "600px",
+            },
+        },
+    },
+    plugins: [
+        require("@tailwindcss/forms"),
+        require("@tailwindcss/typography"),
+        // require("daisyui"),
+        require("flowbite/plugin"),
+    ],
+};
